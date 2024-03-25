664d4c0459d963e374f0a21ec6e50297afcfb2e6
UmairNasirAhmad29 <umair.nasir@esparksinc.com>
2024-03-25 15:01:28 +0500

    url change

--- src/types/Constants.ts
+++ src/types/Constants.ts
@@ -1,13 +1,13 @@
 const prod = {
   url: {
-    API_URL: 'https://staging.lln.lcb.mybluehost.me/public',
-    API_URL_USERS: 'https://staging.lln.lcb.mybluehost.me/public/users',
+    API_URL: 'https://20ef-39-61-50-47.ngrok-free.app',
+    API_URL_USERS: 'https://20ef-39-61-50-47.ngrok-free.app/users',
     PAGE_SIZE: 20
   }
 };
 const dev = {
   url: {
-    API_URL: 'https://staging.lln.lcb.mybluehost.me/public',
+    API_URL: 'https://20ef-39-61-50-47.ngrok-free.app',
     PAGE_SIZE: 20
   }
 };
